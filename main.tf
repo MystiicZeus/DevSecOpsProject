8b94e1890ea54eccc356da23873297724c369644
MystiicZeus <danielnelson1992@gmail.com>
2025-01-25 12:33:54 +0000

    update

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "DevSecOps Project" {
-  bucket = "DevSecOps Project"
+resource "aws_s3_bucket" "dan-n-devsecops-project" {
+  bucket = "dan-n-devsecops-project"
   }
   
